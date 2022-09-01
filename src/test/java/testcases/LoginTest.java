ee358032fce1d200707ff75921eba5e7173ea847
neethu <neethukunchattusuku@gmail.com>
2022-09-02 01:49:56 +0530

    Second Check in

--- src/test/java/testcases/LoginTest.java
+++ src/test/java/testcases/LoginTest.java
@@ -2,14 +2,25 @@ package testcases;
 
 import base.TestBase;
 import org.openqa.selenium.By;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+
 
 public class LoginTest extends TestBase {
 
     @Test
     public void loginAsBankManager() throws InterruptedException {
-        driver.findElement(By.cssSelector(OR.getProperty("bmlbutton"))).click();
-        Thread.sleep(1000);
+        driver.findElement(By.xpath(OR.getProperty("bmlbutton"))).click();
+
+        log.debug("Clicked on the Bank Manager button");
+    }
+
+    @Test
+    public void verifylandingpage() {
+        Assert.assertTrue(isElementPresent(By.xpath(OR.getProperty("Addcustomer"))));
+        driver.findElement(By.xpath(OR.getProperty("Addcustomer"))).click();
+
 
     }
-}
+
+    }
